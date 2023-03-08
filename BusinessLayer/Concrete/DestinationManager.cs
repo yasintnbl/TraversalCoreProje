3d861560da5ec9518824ae7bd0cf84d977a65642
yasintnbl <yasintnbl@gmail.com>
2023-03-08 13:23:29 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager:IDestinationService
+    {
+        IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public List<Destination> TGetlist()
+        {
+            return _destinationDal.GetList();
+        }
+
+        public void TAdd(Destination t)
+        {
+            _destinationDal.Insert(t);
+        }
+
+        public void TDelete(Destination t)
+        {
+            _destinationDal.Delete(t);
+        }
+
+        public Destination TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            _destinationDal.Update(t);
+        }
+    }
+}
